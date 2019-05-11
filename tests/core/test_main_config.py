ea7ea99109487f841e6cc2c6442b8c1b934827b8
BobDylan10 <poubelleab7@gmail.com>
2019-05-12 00:38:42 +0200

    Fix date test

--- tests/core/test_main_config.py
+++ tests/core/test_main_config.py
@@ -40,7 +40,7 @@ class CommonDefaultTestMethodsMixin:
         self.assertEqual('mysql://b3:password@localhost/b3', self.conf.get('b3', 'database'))
         self.assertEqual('b3', self.conf.get('b3', 'bot_name'))
         self.assertEqual('^0(^2b3^0)^7:', self.conf.get('b3', 'bot_prefix'))
-        self.assertEqual('%%I:%%M%%p %%Z %%m/%%d/%%y', self.conf.get('b3', 'time_format'))
+        self.assertEqual('%I:%M%p %Z %m/%d/%y', self.conf.get('b3', 'time_format'))
         self.assertEqual('CST', self.conf.get('b3', 'time_zone'))
         self.assertEqual('9', self.conf.get('b3', 'log_level'))
         self.assertEqual('b3.log', self.conf.get('b3', 'logfile'))
