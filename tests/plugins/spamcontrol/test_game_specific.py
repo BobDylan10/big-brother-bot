661a9cc55150f7cbdd4b5209175d97164cc54bed
BobDylan10 <poubelleab7@gmail.com>
2019-05-12 16:47:51 +0200

    Remove useless time

--- tests/plugins/spamcontrol/test_game_specific.py
+++ tests/plugins/spamcontrol/test_game_specific.py
@@ -32,7 +32,6 @@ from b3.fake import FakeClient
 from b3.plugins.spamcontrol import SpamcontrolPlugin
 from tests.plugins.spamcontrol import SpamcontrolTestCase
 
-@timed(5)
 class Test_game_specific_spam(SpamcontrolTestCase):
 
     def setUp(self):
@@ -52,13 +51,13 @@ class Test_game_specific_spam(SpamcontrolTestCase):
             new_event = Event(type=event.type, client=event.client, target=event.target, data=event.data['text'])
             this.onChat(new_event)
 
-        self.p.onRadio = types.MethodType(onRadio, self.p, SpamcontrolPlugin)
+        self.p.onRadio = types.MethodType(onRadio, self.p)
         self.p.registerEvent('EVT_CLIENT_RADIO', self.p.onRadio)
 
         # patch joe to make him able to send radio messages
         def radios(me, text):
             me.console.queueEvent(Event(type=EVT_CLIENT_RADIO, client=me, data={'text': text}))
-        self.joe.radios = types.MethodType(radios, self.joe, FakeClient)
+        self.joe.radios = types.MethodType(radios, self.joe)
 
     def test_radio_spam(self):
         when(self.p).getTime().thenReturn(0)
